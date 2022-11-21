7f730242f4bc27cc8878cf3946fcc963996cd954
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-21 13:53:22 +0100

    Product adding function created.

---
+++ 5/solution.js
@@ -0,0 +1,29 @@
+let productsList = [];
+
+/**
+ * Adds product to products list.
+ * @param {string} name - Product name.
+ * @param {number} number - Product quantity.
+ * @param {Date} buyDate - Product buy date.
+ * @param {boolean} bought - Is product bought already.
+ * @param {number} itemPrice - Product item price.
+ * @returns {number} Product id.
+ */
+function addProduct(name, number, buyDate, bought, itemPrice) {
+    const id = Math.floor(Math.random() * 1000000000);
+    productsList.push({
+        id: id,
+        name: name,
+        number: number,
+        buyDate: buyDate,
+        bought: bought,
+        itemPrice: itemPrice
+    })
+    return id;
+}
+
+console.log(productsList);
+
+addProduct("Product 1", 2, Date.now(), false, 20.0);
+
+console.log(productsList);
