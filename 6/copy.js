c4bcd3d33772ff58460824c24d856f67a7845ff7
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-28 13:17:01 +0100

    CloneDeep nested object copy added.

--- 6/copy.js
+++ 6/copy.js
@@ -1,4 +1,30 @@
 function cloneDeep(obj) {
     let newObj = {...obj};
+    for(const prop in newObj) {
+        if(typeof newObj[prop] === "object") {
+            newObj[prop] = cloneDeep(newObj[prop]);
+        }
+    }
     return newObj;
 }
+
+const user = {
+    name: "Alice",
+    surname: "Young",
+    contact: {
+      phone: '111-111-111',
+      address: {
+        city: "London",
+      }
+    }
+  };
+  
+const userCp = cloneDeep(user);
+  
+user.name = "John";
+user.surname = "White";
+user.contact.phone = '222-222-222';
+user.contact.address.city = 'New York'
+  
+console.log(user);
+console.log(userCp);
