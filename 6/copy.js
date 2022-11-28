048391d04eeeb97e5f9232468ce54b1f47fc0f09
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-28 13:30:42 +0100

    CloneDeep Date object copy added.

--- 6/copy.js
+++ 6/copy.js
@@ -1,7 +1,9 @@
 function cloneDeep(obj) {
     let newObj = {...obj};
     for(const prop in newObj) {
-        if(typeof newObj[prop] === "object") {
+        if(newObj[prop] instanceof Date) {
+            newObj[prop] = new Date(obj[prop].getTime());
+        } else if(typeof newObj[prop] === "object") {
             newObj[prop] = cloneDeep(newObj[prop]);
         }
     }
@@ -11,18 +13,24 @@ function cloneDeep(obj) {
 const user = {
     name: "Alice",
     surname: "Young",
+    dates: {
+        birthDate: new Date(1995, 10, 28)
+    },
     contact: {
-      phone: '111-111-111',
-      address: {
-        city: "London",
-      }
+        phone: '111-111-111',
+        address: {
+            city: "London",
+        }
     }
-  };
-  
+};
+
+console.log(user);
+
 const userCp = cloneDeep(user);
   
 user.name = "John";
 user.surname = "White";
+user.dates.birthDate = new Date(1999, 11, 11);
 user.contact.phone = '222-222-222';
 user.contact.address.city = 'New York'
   
